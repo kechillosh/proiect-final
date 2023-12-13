ceee40a1c219db2f262a8da7cc6ee62bec5ccd14
Kechillosh <amir.elshaer13@gmail.com>
2023-12-13 21:48:24 +0200

    Update 1.2

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,22 +1,59 @@
 <template>
   <nav>
     <div>
-      <router-link to="/Home" class="nav-link"><Icon icon="material-symbols:home"/>Home</router-link>
-      <router-link to="/MyRuns" class="nav-link"><Icon icon="cil:running"/>MyRuns</router-link>
-      <router-link to="/Profile" class="nav-link"><Icon icon="iconamoon:profile-fill"/>Profile</router-link>
+      <router-link to="/Home" class="nav-link">
+        <Icon icon="material-symbols:home"/>
+        Home
+      </router-link>
+      <router-link to="/MyRuns" class="nav-link">
+        <Icon icon="cil:running"/>
+        MyRuns
+      </router-link>
+      <router-link to="/Profile" class="nav-link">
+        <Icon icon="iconamoon:profile-fill"/>
+        Profile
+      </router-link>
+      <a class="icon-link" @click="toggleList">
+        <Icon
+            class="user"
+            icon="iconamoon:profile-circle-duotone"
+            width="30"
+            color="white"
+        />
+      </a>
+      <div v-if="showList" class="dropdown-list">
+        <!-- Your list content goes here -->
+        <router-link to="/Logout" class="dropdown-item">Logout</router-link>
+      </div>
     </div>
   </nav>
 </template>
 
 <script>
 import { Icon } from '@iconify/vue';
+
 export default {
   name: "Navbar",
-  components: {Icon}
+  components: { Icon },
+  data() {
+    return {
+      showList: false,
+    }
+  },
+  methods: {
+    toggleList() {
+      this.showList = !this.showList;
+    },
+  },
 }
 </script>
 
+
 <style scoped>
+.user {
+  margin-left: 75vw;
+}
+
 nav {
   background-color: #333; /* Dark Gray */
   padding: 10px 20px;
@@ -43,4 +80,14 @@ div {
   color: #2ecc71; /* Lime Green */
 }
 
+
+.icon-link {
+  text-decoration: none;
+  transition: opacity 0.3s ease-in-out;
+}
+
+.icon-link:hover {
+  opacity: 0.75;
+  cursor: pointer;
+}
 </style>
